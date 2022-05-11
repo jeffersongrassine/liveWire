8b9e8b5f0c7f2e0ff8b52f7486919b5e1c717d4a
jeffersongrassine <jeffersongrassine@gmail.com>
2022-05-11 13:08:55 -0300

    Includ Pagination

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -4,15 +4,19 @@ namespace App\Http\Livewire;
 
 use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowTweets extends Component
 {
-    
-    public $message = 'Apenas um teste';
+    // Traid para não FAZER RELOAD NA PAGINAÇÃO
+    use WithPagination;
+
 
     public function render()
     {
-        $tweets = Tweet::with('user')->get(); //Metodo with(), otimiza busca ao BD
+        //Pega os tweeter do usuario
+        // $tweets = Tweet::with('user')->get(); //Metodo with(), otimiza busca ao BD
+        $tweets = Tweet::with('user')->paginate(3); //Metodo with(), otimiza busca ao BD
 
         return view('livewire.show-tweets', [ 
             'tweets' => $tweets
@@ -20,4 +24,27 @@ class ShowTweets extends Component
         ]);
 
     }
+    
+    public $content = 'Apenas um teste';
+
+    // Validações
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
+    
+    public function create()
+    {
+        $this->validate();
+
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => 1,
+        ]);
+
+        $this->content = '';
+        
+        //dd($this->message);
+    }
+
+   
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -2,9 +2,16 @@
    Show Tweets
 
 
-   <p>{{ $message }}</p> 
+   <p>{{ $content }}</p> 
 
-   <input type="text" name="message" id="message" wire:model="message" />
+   <form method="post" wire:submit.prevent="create">
+
+       <input type="text" name="content" id="content" wire:model="content" />
+       @error('content')
+           {{ $message}}
+       @enderror
+       <button type="submit">Criar Tweet</button>
+   </form>
    <hr>
 
    @foreach ($tweets as $tweet)
@@ -13,4 +20,9 @@
 
     @endforeach
 
+
+    <hr>
+    <div>
+        {{ $tweets->links()}}
+    </div>
 </div>
